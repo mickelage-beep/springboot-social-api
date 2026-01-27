791677bfedc1470d33172cd1ea9252350392c7e1
aliwa <ali.wafa17943@gmail.com>
2026-01-27 14:11:16 +0100

    ren

--- src/main/java/se/jensen/mikael/springboot/config/WebConfig.java
+++ src/main/java/se/jensen/mikael/springboot/config/WebConfig.java
@@ -1,5 +1,6 @@
 package se.jensen.mikael.springboot.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
